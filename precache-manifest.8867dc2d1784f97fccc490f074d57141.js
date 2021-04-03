722631fa54241b1b92dd06c21c7c9aa79ee57f4c
Andres Fuentes <andresfuentes@towerhousestudio.com>
2021-04-03 15:28:08 -0400

    Updates

---
+++ precache-manifest.8867dc2d1784f97fccc490f074d57141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301c4a443a019cbe27bb3bcc3d0ba077",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "02a0375575add218e8b2",
+    "url": "/drum-machine/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "5e0610cf58c74cd7832a",
+    "url": "/drum-machine/static/css/main.4db6bb6a.chunk.css"
+  },
+  {
+    "revision": "02a0375575add218e8b2",
+    "url": "/drum-machine/static/js/2.38c69afa.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/drum-machine/static/js/2.38c69afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0610cf58c74cd7832a",
+    "url": "/drum-machine/static/js/main.83250d95.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
